6b38b850a17b72ae7907a2e04461d64ded37148b
yipho97 <sengyipho_97@hotmail.com>
2023-04-25 00:16:40 +0800

    Login and Signup modals

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 const font = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
@@ -16,6 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
